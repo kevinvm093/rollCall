61e0c13fe9198988119445c06126e246c42aa339
kevinvm093 <kvallejo093@me.com>
2018-02-18 17:48:34 -0500

    Initial Commit

---
+++ rollCall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rollCall
+//
+//  Created by Kevin Vallejo on 2/18/18.
+//  Copyright © 2018 Vallejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
